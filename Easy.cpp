1e08649147692d8420a63bbb93f62a868a2ff793
Joseph Piccoli <joe13676@comcast.net>
2020-11-14 10:41:48 -0800

    Add the solution for problem #456
    Add the solution for problem #456

--- Easy.cpp
+++ Easy.cpp
@@ -4,6 +4,83 @@
 //{
 //}
 
+struct action
+{
+  action(long timestamp_, int count_, std::string type_) : timestamp(timestamp_), count(count_), type(type_)
+  {}
+
+  long timestamp;
+  int count;
+  std::string type;
+};
+
+std::pair<long, long> number_456()
+{
+  std::pair<long, long> times;
+
+  int current_count = 0;
+  int max_count = 0;
+  int min_count = 0;
+  int min_time = 0;
+  int num_people = 0;
+  int max_num_people = 0;
+  long max_time = 0;
+
+  std::vector<action> actions = { 
+    action(1526579928, 3, "enter"),
+    action(1526580382, 2, "exit"),
+    action(1526581000, 4, "enter"),
+    action(1526581003, 3, "exit"),
+  };
+
+  //for(auto const& item : actions)
+  //{
+  //  if(item.type == "enter")
+  //  {
+  //    current_count += item.count;
+  //    if(max_count == 0 || max_count < current_count)
+  //    {
+  //      max_count = current_count;
+  //      max_time = item.timestamp;
+  //    }
+  //    if(min_count == 0 || current_count < min_count)
+  //    {
+  //      min_count = current_count;
+  //      min_time = item.timestamp;
+  //    }
+  //  }
+  //  else if(item.type == "exit")
+  //  {
+  //    current_count -= item.count;
+  //    if(current_count < min_count)
+  //    {
+  //      min_time = item.timestamp;
+  //    }
+  //  }
+  //}
+
+  for(unsigned i = 0; i < actions.size() - 1; ++i)
+  {
+    if(actions[i].type == "enter")
+    {
+      num_people += actions[i].count;
+    }
+    else if(actions[i].type == "exit")
+    {
+      num_people -= actions[i].count;
+    }
+
+    if(max_num_people < num_people)
+    {
+      max_num_people = num_people;
+      times.first = actions[ i ].timestamp;
+      times.second = actions[ i + 1 ].timestamp;
+    }
+  }
+
+  return times;
+}
+
 bool number_491(int value)
 {
   auto temp = value;
--- Easy.h
+++ Easy.h
@@ -2,8 +2,11 @@
 
 #include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
+std::pair<long, long> number_456();
 std::vector<int> number_489(std::vector<int> arr);
 bool number_491(int value);
 
